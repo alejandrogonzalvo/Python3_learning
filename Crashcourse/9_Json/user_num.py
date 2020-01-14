086c583e6b5c4d81e5ad32f0807e73c29d0247a0
dahko37 <alejandrogonhid@gmail.com>
2020-01-14 18:50:45 +0100

    comentando programas

--- Crashcourse/9_Json/user_num.py
+++ Crashcourse/9_Json/user_num.py
@@ -1,7 +1,11 @@
 # !/usr/bin/python3.5
  
+# This program executes asks a user for their name and favourite number and stores it in a json file.
+# The program should work correctly withour editing the file. If it doesn't try checking if the json file exists but it's empty. if so delete it
+
 
 import favnum as f
+
 
 name = f.greet_user()
 f.favnum(name)
