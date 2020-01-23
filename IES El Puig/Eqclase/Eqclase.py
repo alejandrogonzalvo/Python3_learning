d5cf9794c8298e019d5e10928460343384f358b8
dahko37 <alejandrogonhid@gmail.com>
2020-01-23 18:33:14 +0100

    eqclase

--- IES El Puig/Eqclase/Eqclase.py
+++ IES El Puig/Eqclase/Eqclase.py
@@ -1,35 +1,59 @@
 # !/usr/bin/python3.5
 
 
-import math
-import time
-
-
 class Eqsegundogrado:
-    """Estructura de datos con formato de equación de segundo grado y funciones de interacción con sus respectivos algoritmos."""
+    """
+
+    Estructura de datos con formato de equación de segundo grado y funciones
+    de interacción con sus respectivos algoritmos.
+    
+    """
 
 
     def __init__(self, a = 0, b = 0, c = 0):
+        """
+
+        Asignamos valores, y mostramos la equación actual.
+
+        """
 
         self.a = a
-        print (f"el valor de a es {self.a}")
+        print (f"\nel valor de a es {self.a}")
 
         self.b = b
-        print (f"el valor de b es {self.b}")
+        print (f"\nel valor de b es {self.b}")
 
         self.c = c
-        print (f"el valor de c es {self.c}")
+        print (f"\nel valor de c es {self.c}")
 
         self.dis = self.b*self.b-4*self.a*self.c
 
-        print(f"\n{self.a}x*x + {self.b}x + {self.c} es la nueva equación")
+        self.sol1 = (-self.b + self.dis**0.5)/2
+        self.sol2 = (-self.b - self.dis**0.5)/2
+
+        self.eq = f"{self.a}x*x + {self.b}x + {self.c}"
+
+
+
+        print(f"\n{self.eq} es la nueva equación")
 
 
     def __str__(self):
-        return '{self.a}x*x + {self.b}x + {self.c} es la equación actual'.format(self=self)
+        """
+
+        Contenido cuando hagamos print() a un objeto de clase Eqsegundogrado.
+
+        """
+
+        return f'{self.eq} es la equación actual'.format(self=self)
+
 
     def redef(self):
-        """Recopila el valor de las variables de la equacion"""
+        """
+        
+        Recopila el valor de las variables de la equacion
+        
+        """
 
         self.a = int(input("introduzca el nuevo valor de la variable a:"))
         print (f"el valor de a es {self.a}")
@@ -40,72 +64,66 @@ class Eqsegundogrado:
         self.c = int(input("introduzca el nuevo valor de la variable c:"))
         print (f"el valor de a es {self.c}")
 
-        print(f"{self.a}x*x + {self.b}x + {self.c} es la nueva equación")
+        print(f"{self.eq} es la nueva equación")
+
 
     def tiposol(self):
+        """
+
+        Devuelve el tipo de solución
+
+        """
 
         if self.dis > 0:
             print("La equacion tiene dos soluciones reales.")
 
-        elif self.dis = 0:
+        elif self.dis == 0:
             print("La equacion solo tiene una solucion real (0).")
 
         else:
             print("La equacion tiene 2 soluciones imaginarias.")
 
 
-    def resolver(self):
-    
-        if self.dis >= 0:
-            sol1 = (-self.b + math.sqrt(self.dis))/2
-            sol2 = (-self.b - math.sqrt(self.dis))/2
+    def mostrarsol(self):
+        """
 
-            print (f"\nel valor de la solución 1 es {sol1}\n")
-            print (f"\nel valor de la solución 2 es {sol2}\n")
+        Muestra las soluciones de la equación.
 
-        else:
-            print("\nla solucion a la equación introducida es un número complejo\n")
+        """
+
+        print (f"\nel valor de la solución 1 es {self.sol1}\n")
+        print (f"\nel valor de la solución 2 es {self.sol2}\n")
+
 
     def sollista(self):
+        """
 
-        lista = []
+        Devuelve las soluciones como una lista
 
-        if self.dis >= 0:
-            sol1 = (-self.b + math.sqrt(self.dis))/2
-            sol2 = (-self.b - math.sqrt(self.dis))/2
+        """
 
-        else:
+        lista = [self.sol1, self.sol2]
 
-            sol1 = "c1"
-            sol2 = "c2"
-
-        lista.append(sol1)
-        lista.append(sol2)
         return lista
 
+
     def soltupla(self):
+        """
 
-        tupla = []
+        Devuelve las soluciones como una tupla
 
-        if self.dis >= 0:
-            sol1 = (-self.b + math.sqrt(self.dis))/2
-            sol2 = (-self.b - math.sqrt(self.dis))/2
+        """
 
-        else:
+        tupla = (self.sol1, self.sol2)
 
-            sol1 = "c1"
-            sol2 = "c2"
-
-        tupla.append(sol1)
-        tupla.append(sol2)
         return tupla
 
 
 
 
-eq = Eqsegundogrado(2, 4, 1)
+eq = Eqsegundogrado(8, 4, 1)
     
-eq.mostrar()
+print(eq)
 eq.redef()
-eq.mostrar()
-eq.resolver()
+print(eq)
+eq.mostrarsol()
