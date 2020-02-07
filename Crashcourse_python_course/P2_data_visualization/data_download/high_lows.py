24edf61c03ffb3429efa5ed8a795defa4ca43b1f
dahko37 <alejandrogonhid@gmail.com>
2020-02-07 17:00:55 +0100

    a

--- Crashcourse_python_course/P2_data_visualization/data_download/high_lows.py
+++ Crashcourse_python_course/P2_data_visualization/data_download/high_lows.py
@@ -1,28 +1,39 @@
 import csv
+from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-path_1 = '/home/alejandrogonzalvo4eso/Escritorio/python3_learning'
-path_2 = '/Crashcourse_python_course/P2_data_visualization/data_download'
-path_3 = '/resources/sitka_weather_07-2014.csv'
-filename = path_1 + path_2 + path_3
+
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    highs = []
+    dates, highs, lows = [], [], []
     for row in reader:
+        date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(date)
+
         high = int(row[1])
         highs.append(high)
+
+        low = int(row[3])
+        lows.append(low)
 
     print(highs)
 
 
 fig = plt.figure(dpi = 128, figsize = (10,6))
-plt.plot(highs, c = 'red')
 
-plt.title("Daily high temperatures, July 2014", fontsize = 24)
-plt.xlabel('', fontsize = 16)
+plt.plot(dates, highs, c = 'red', alpha = 0.5)
+plt.plot(dates, lows, c = 'blue', alpha = 0.5)
+plt.fill_between(dates, highs, lows, facecolor = 'red', alpha = 0.3)
+plt.fill_between(dates, 0, lows, facecolor = 'blue', alpha = 0.3)
+
+csfont = {'fontname':'Comic Sans MS'}
+plt.title("Daily temperatures, 2014", fontsize = 24, **csfont)
+plt.xlabel('', fontsize = 16, **csfont)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize = 16)
 plt.tick_params(axis = 'both', which = 'major', labelsize = 16)
 
