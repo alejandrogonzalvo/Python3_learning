a9a233b5fc165597749abee8c262279d16aa9d23
dahko37 <alejandrogonhid@gmail.com>
2020-02-26 14:36:52 +0100

    move file

--- file_mover/main.py
+++ file_mover/main.py
@@ -30,7 +30,7 @@ def arrange_files(path):
         except shutil.Error:
              pass
 
-    folders = ["Imagenes", "Videos", "Audio", "Ebooks", "pdf"]
+    folders = ["Imagenes", "Videos", "Audio", "Ebooks", "pdf", "rar", "otros"]
     files = os.listdir(path)
     for f in files:
         
@@ -41,11 +41,23 @@ def arrange_files(path):
             if imghdr.what(path + f) != None:
                 move_file(folders[0])
 
+            elif ext == ".avi" or ext == '.mov' or ext == '.mp4':
+                move_file(folders[1])
+
+            elif ext == ".mp3" or ext == ".wav":
+                move_file(folders[2])
+
             elif ext == ".azw3":
                 move_file(folders[3])
             
             elif ext == ".pdf":
                 move_file(folders[4])
+
+            elif ext == ".rar":
+                move_file(folders[5])
+
+            else:
+                move_file(folders[6])
 
 
 
