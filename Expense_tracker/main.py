27f5280909fe7e99f9918504236b7426a3b0d5d9
dahko37 <alejandrogonhid@gmail.com>
2020-02-28 16:50:53 +0100

    creando expense_tracker

--- Expense_tracker/main.py
+++ Expense_tracker/main.py
@@ -24,9 +24,6 @@ def initialize(filename):
     except FileNotFoundError: #If the file doesn't exist, we create it.
         print("Seems you are the first one here!")
 
-        with open(filename, 'w') as f_obj:
-            pass
-
         return budgets
      
 
@@ -38,9 +35,8 @@ class Account:
     Simulates a budget with custom percentatges for each area of spending
 
     """
-    def __init__(self, name, cash = "0", percent):
-        self.name = name
-        self.cash = cash
+    def __init__(self, cash, percent):
+        self.cash = int(cash)
         self.percent = percent
         self.budget = {}
             
@@ -111,8 +107,8 @@ def create():
     percent = {}
     
     while True:
-        key = input("Name a new category")
-        val = input("Select the percentatge (1-100) for the new category")
+        key = input("Name a new category: ")
+        val = int(input("Select the percentatge (1-100): "))
         percent[key] = val
         total_val = 0
 
@@ -130,6 +126,31 @@ def create():
         elif total_val > 100:
             print("The percentatge cannot be greater than 100. Restarting")
             total_val = 0
+            percent.clear()
+            continue
+
+        return name, cash, percent
+
+
+def select(budgets):
+    """Selects an existing account to work with"""
+    accname = input("Select an existing account: ")
+
+    if accname in budgets:
+        print('account succesfully changed')
+        selacc = budgets(accname)
+        return selacc
+
+    else:
+        print('Sorry, the selected account doesnt exist')
+
+
+def save(filename, budgets):
+    """Saves the changes made into the accounts"""
+
+    with open(filename, 'w') as f_obj: 
+        json.dump(budgets, f_obj)
+    
 
 
 def main():
@@ -145,10 +166,33 @@ def main():
         if a == 'h':
             help()
 
-        elif a == 'z':
-            break
+        elif a == 'create':
+            while True:
+                accname, cash, percent = create()
 
-        elif a == 's':
+                if accname in budgets:
+                    print('Sorry, there is already an account with this name. ')
+                    continue
+
+                break
+            
+            accname = Account(cash, percent)
+            print('Account created succesfully')
+            budgets.append(accname)
+
+        elif a == 'select':
+            selacc = select(budgets)
+            
+        elif a == 'show':
+            for key, value in selacc.budget:
+                print(key + ' : ' + value)
+
+        elif a == 'save':
+            save(filename, budgets)
+            print('account saved succesfully')
+        
+        elif a == 'exit':
+            break
 
         else:
             print("ERROR: The input is not an operation\n")
