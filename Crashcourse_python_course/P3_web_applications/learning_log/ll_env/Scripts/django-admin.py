ec0ad53db61311ea774ce6b6ae9a670d58f896da
dahko37 <alejandrogonhid@gmail.com>
2020-03-16 19:21:18 +0100

    borrar web apps

--- Crashcourse_python_course/P3_web_applications/learning_log/ll_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\alexf\documents\mypython3\crashcourse_python_course\p3_web_applications\learning_log\ll_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
