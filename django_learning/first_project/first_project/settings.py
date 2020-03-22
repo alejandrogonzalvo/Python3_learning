5b8d7a63f78c580ef31d3737f6e6d9e9d4cee4ae
dahko37 <alejandrogonhid@gmail.com>
2020-03-22 09:01:00 +0100

    empezando con django, entornos virtuales y bases de datos

--- django_learning/first_project/first_project/settings.py
+++ django_learning/first_project/first_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'first_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'first_project',
+        'USER': 'dahko37',
+        'PASSWORD': 'alexis024680',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
