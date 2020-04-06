0bb528ca6852e87b361c13b5a0e34662ad9f6587
dahko37 <alejandrogonhid@gmail.com>
2020-04-06 10:08:21 +0200

    Detenemos proyecto porque vamos muy perdidos y hay que estudiar mas

--- django_learning/chatbox/chat/urls.py
+++ django_learning/chatbox/chat/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from .views import ConversationListView
+from .views import ConversationListView, MessageListView
 
 
 app_name = 'chat'
-urlpatterns =[
-    path('', ConversationListView.as_view(), name='conversation_list')
+urlpatterns = [
+    path(
+        '',
+        ConversationListView.as_view(),
+        name='conversation_list'),
+
+    path(
+        '<str:conversation>',
+        MessageListView.as_view(),
+        name='conversation')
 ]
--- django_learning/chatbox/chat/views.py
+++ django_learning/chatbox/chat/views.py
@@ -9,14 +9,24 @@ class ConversationListView(generic.ListView):
     """
     Represents the visualization of a conversation.
     """
+    template_name = 'templates/chat/conversation_list.html'
+    context_object_name = 'conversation_list'
 
-    model = Conversation
-
-    def get_conversation_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['now'] = timezone.now()
-        return context
+    def get_queryset(self):
+        return Conversation.objects.all()
 
 
 class MessageListView(generic.ListView):
-    #TO DO
+    """
+    Represents the visualization of a list of messages from
+    a specific conversation.
+    """
+
+    model = Message
+    template_name = 'templates/chat/message_list.html'
+    context_object_name = 'message_list'
+
+    def get_queryset(self):
+        return Message.objects.filter(
+            conversation=self.kwargs['conversation_id']
+            )
